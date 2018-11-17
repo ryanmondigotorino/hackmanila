01087e38a21609de53a628193d2480a5d5346719
Christine Lee <lee.christinedazo@gmail.com>
2018-11-17 14:20:38 +0800

    Updated Css

--- application/views/landing/home.php
+++ application/views/landing/home.php
@@ -1,1 +1,68 @@
-hash_copy <a href="<?=base_url()?>landing/getlanding">link to get</a>
+<section class="banner-1">
+    <div class="container">
+          <div class="row">
+            <div class="col-md-6 product">
+                <p class="product-title">
+                Lorem Ipsum
+                </p>
+                <p class="product-content">
+                is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                </p>
+            </div>
+           
+        </div>
+    </div>
+</section>
+<section class="product">
+    <div class="container">
+        <p class="acc-title"> FEATURED ACCESSORIES</p>
+        <div class="row">
+            <div class="col-sm-4 product-wrapper-small">
+            <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+                <p class="product-details">
+                Lorem Ipsum
+                </p>
+            </div>
+            <div class="col-sm-8 product-wrapper">
+                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+                <p class="product-details">
+                Lorem Ipsum
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 product-wrapper">
+                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+                <p class="product-details">
+                Lorem Ipsum
+                </p>
+            </div>
+            <div class="col-sm-4 product-wrapper">
+                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+                <p class="product-details">
+                Lorem Ipsum
+                </p>
+            </div>
+            <div class="col-sm-4 product-wrapper">
+                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+                <p class="product-details">
+                Lorem Ipsum
+                </p>
+            </div>
+        </div>
+        <a href="" class="shop-link">Shop iTech accessories</a>
+    </div>
+</section>
+<section class="new_arrivals">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 new-arr-title">
+                <p class="bold">NEW ARRIVALS</p>
+                <p class="product-content">Check out iTech's new accessories</p>
+                <a href="#" class="product-content">Shop Now</a>
+            </div>
+        </div>
+    </div>
+</section>
