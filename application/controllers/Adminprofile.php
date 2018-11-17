6d25fdc218a5a94497d0de04a943f936a12bf5db
Ryan <ryan.torino.a99ojt@gmail.com>
2018-11-17 14:14:00 +0800

    Create Dashboard Adminprofile files

---
+++ application/controllers/Adminprofile.php
@@ -0,0 +1,18 @@
+<?php
+
+class Adminprofile extends CI_Controller{
+
+    function __construct() {
+        parent::__construct();
+        date_default_timezone_set("Asia/Manila");
+        
+    }
+    public function index(){
+        $title = array(
+            'title' => 'Home'
+        );
+        $this->load->view('landing/includes/header',$title);
+        $this->load->view('landing/home');
+        $this->load->view('landing/includes/footer');
+    }
+}
