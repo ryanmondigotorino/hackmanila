35d6cff2fc1eb1051d9b5a1bafee92744e66cd86
Christine Lee <lee.christinedazo@gmail.com>
2018-11-17 21:23:45 +0800

    Updated shop page

--- application/views/landing/includes/header.php
+++ application/views/landing/includes/header.php
@@ -5,14 +5,15 @@
     <link rel="stylesheet" href="<?=base_url()?>assets/css/custom.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400i" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-sm navbar-light justify-content-between">
-        <a class="navbar-brand" href="#"><img src=""></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown">
             <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse flex-grow-0" id="navbarDropdown">
             <ul class="navbar-nav text-right">
--- application/views/landing/shop.php
+++ application/views/landing/shop.php
@@ -1,127 +1,148 @@
 <section class="banner-signup">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 product">
-                <p class="product-title">
-                    SHOP
-                </p>
-                <p class="product-content">
-                    Your one click application to buy anything you want!
-                </p>
-            </div>
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 product">
+            <p class="product-title">
+                SHOP
+            </p>
+            <p class="product-content">
+                Your one click application to buy anything you want!
+            </p>
         </div>
+
     </div>
+</div>
 </section>
 <section class="product-wrap">
-    <div class="container">
-        <p class="dark-text">
-            EXCLUSIVE FOR YOU
-        </p>
-        <div class="row">
-            <div class="col-sm-4 product-wrapper-small">
-                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
-                <p class="product-details">
-                    Lorem Ipsum
-                </p>
-                <div class="middle">
-                    <div class="product-details">
-                        <button class="btn btn-dark" data-toggle="modal" data-target=".modal-1">SHOP NOW</button>
-                        <!-- add a unique per button *loop -->
-                    </div>
+<div class="container">
+    <p class="dark-text">
+        EXCLUSIVE FOR YOU
+    </p>
+    <div class="row">
+        <div class="col-sm-4 product-wrapper-small">
+            <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+            <p class="product-details">
+                Lorem Ipsum
+            </p>
+            <div class="middle">
+                <div class="product-details">
+                    <button class="btn btn-dark" data-toggle="modal" data-target=".modal-1">SHOP NOW</button>
+                    <!-- add a unique per button *loop -->
                 </div>
-                <!-- MODAL -->
-                <div class="modal fade modal-1" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-
-                        </div>
-                    </div>
-                </div>
-                <!-- END OF MODAL -->
             </div>
-            <div class="col-sm-4 product-wrapper-small">
-                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
-                <p class="product-details">
-                    Lorem Ipsum
-                </p>
-                <div class="middle">
-                    <div class="product-details">
-                        <button class="btn btn-dark" data-toggle="modal" data-target=".modal-2">SHOP NOW</button>
-                        <!-- add a unique per button *loop -->
-                    </div>
-                </div>
-                <!-- MODAL -->
-                <div class="modal fade modal-2" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
+            <!-- MODAL -->
+            <div class="modal fade modal-1" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
 
-                        </div>
                     </div>
                 </div>
-                <!-- END OF MODAL -->
             </div>
-            <div class="col-sm-4 product-wrapper-small">
-                <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
-                <p class="product-details">
-                    Lorem Ipsum
-                </p>
-                <div class="middle">
-                    <div class="product-details">
-                        <button class="btn btn-dark" data-toggle="modal" data-target=".modal-3">SHOP NOW</button>
-                        <!-- add a unique per button *loop -->
+            <!-- END OF MODAL -->
+        </div>
+        <div class="col-sm-4 product-wrapper-small">
+            <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+            <p class="product-details">
+                Lorem Ipsum
+            </p>
+            <div class="middle">
+                <div class="product-details">
+                    <button class="btn btn-dark" data-toggle="modal" data-target=".modal-2">SHOP NOW</button>
+                    <!-- add a unique per button *loop -->
+                </div>
+            </div>
+            <!-- MODAL -->
+            <div class="modal fade modal-2" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
                     </div>
                 </div>
-                <!-- MODAL -->
-                <div class="modal fade modal-3" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="container expanded">
-                                            <img id="expandedImg" style="width:100%">
-                                            <div id="imgtext"></div>
+            </div>
+            <!-- END OF MODAL -->
+        </div>
+        <div class="col-sm-4 product-wrapper-small">
+            <img class="product-img" src="<?=base_url()?>/assets/images/cases.png" alt="">
+            <p class="product-details">
+                Lorem Ipsum
+            </p>
+            <div class="middle">
+                <div class="product-details">
+                    <button class="btn btn-dark" data-toggle="modal" data-target=".modal-3">SHOP NOW</button>
+                    <!-- add a unique per button *loop -->
+                </div>
+            </div>
+            <!-- MODAL -->
+            <div class="modal fade modal-3" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-6 expanded">
+                                    <div class="container">
+                                        <img id="expandedImg" src="<?=base_url()?>/assets/images/ear-buds-png-6.png" style="width:100%">
+                                        <div id="imgtext"></div>
+                                    </div>
+                                    <!-- The four columns -->
+                                    <div class="row">
+                                        <div class="column">
+                                            <img src="<?=base_url()?>/assets/images/earphones-1.png" style="width:100%" onclick="productImg(this);">
                                         </div>
-                                        <!-- The four columns -->
-                                        <div class="row">
-                                            <div class="column">
-                                                <img src="<?=base_url()?>/assets/images/cases.png" alt="Nature" style="width:100%" onclick="productImg(this);">
-                                            </div>
-                                            <div class="column">
-                                                <img src="<?=base_url()?>/assets/images/apple-watch.jpg" alt="Mountains" style="width:100%" onclick="productImg(this);">
-                                            </div>
-                                            <div class="column">
-                                                <img src="<?=base_url()?>/assets/images/apple-watch.jpg" alt="Lights" style="width:100%" onclick="productImg(this);">
-                                            </div>
+                                        <div class="column">
+                                            <img src="<?=base_url()?>/assets/images/earphones-2.jpg" style="width:100%" onclick="productImg(this);">
+                                        </div>
+                                        <div class="column">
+                                            <img src="<?=base_url()?>/assets/images/ear-buds-png-6.png" style="width:100%" onclick="productImg(this);">
                                         </div>
                                     </div>
-                                    <div class="col-sm-6">
-                                        <p class="prod-title">Lorem Ipsum</p>
-                                        <p class="prod-details">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                        </p>
+                                </div>
+                                <div class="col-sm-6 expanded-content">
+                                    <p class="prod-title">Earphones</p>
+                                    <p class="prod-details">QC20 noise cancelling headphones – Samsung/Android™ devices</p>
+                                    <p class="price">P179.00</p>
+                                    <div class="input-group">
+                                        <span class="input-group-btn">
+                                            <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                        </span>
+                                        <input type="text" name="quant[1]" class="form-control input-number" value="1" min="1" max="10">
+                                        <span class="input-group-btn">
+                                             <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
+                                        <i class="fa fa-plus"></i>
+                                        </button>
+                                        </span>
                                     </div>
+
+                                </div>
+                            </div>
+                            <div class="row buttons">
+                                <div class="col-md-3">
+                                    <a href=""><button class="btn btn-primary">Buy Now</button></a>
+                                </div>
+                                <div class="col-md-3">
+                                     <a href=""><button class="btn btn-warning">Add to Chart</button></a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- END OF MODAL -->
             </div>
+            <!-- END OF MODAL -->
         </div>
     </div>
+</div>
 
 </section>
 
 <section class="new_arrivals">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 new-arr-title">
-                <p class="bold">GET 10% DISCOUNT
-                    <br>WHEN YOU BUY</p>
-                <p class="product-content">Check out now our new items!</p>
-                <a href="#" class="product-content">Shop Now</a>
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 new-arr-title">
+            <p class="bold">GET 10% DISCOUNT
+                <br>WHEN YOU BUY</p>
+            <p class="product-content">Check out now our items!</p>
         </div>
     </div>
+</div>
 </section>
