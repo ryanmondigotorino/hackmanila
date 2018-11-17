09a462fe603d1b3618749da353b7c3953472331b
Ryan <ryan.torino.a99ojt@gmail.com>
2018-11-17 14:10:45 +0800

    add Controller Connect mobile

---
+++ application/controllers/Connectmobile.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connectmobile extends CI_Controller{
+
+    function __construct() {
+        parent::__construct();
+        date_default_timezone_set("Asia/Manila");
+        
+    }
+
+    public function index(){
+        
+    }
+}
